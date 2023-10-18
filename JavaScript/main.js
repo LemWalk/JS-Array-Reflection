b64e27b9bb1f8ee8e515f6cab9ba6efe56e0d42c
LemWalk <lemuel.walkinshaw@netmatters-scs.com>
2023-10-18 13:03:20 +0100

    *new img btn working*

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,1 +1,23 @@
-console.log('Welcome to the DOJO Rookie!')
+const btnNewImage = document.getElementById('btn-new-img');
+const url = fetch("https://picsum.photos/200");
+let img = document.getElementById('fetched-image');
+const imgBox = document.getElementById('img-box');
+
+//========== Fetch a new image when the 'new image' button is clicked ================
+
+    btnNewImage.addEventListener("click", () => {
+    fetch('https://picsum.photos/200')
+    .then(response => response.blob())
+    .then(blob => {
+    const objectURL = URL.createObjectURL(blob);
+    img.src = objectURL;
+    imgBox.appendChild(img);
+    });
+        });
+
+
+
+
+
+// regular expression for email
+// ^[a-z]+@[^\s]+\.[^\s]+$
